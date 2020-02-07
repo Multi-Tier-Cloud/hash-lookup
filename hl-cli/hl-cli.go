be2f5d2990f6ae88e945b03ba4e1c505bf600f45
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-02-07 18:25:20 -0500

    hashing file, directory, or stdin content

--- hl-cli/hl-cli.go
+++ hl-cli/hl-cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -29,70 +30,30 @@ var commands = map[string]func(){
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Missing command")
+		fmt.Fprintln(os.Stderr, "Missing command")
+		usage()
 		return
 	}
 
 	cmdFunc, ok := commands[os.Args[1]]
 	if !ok {
-		fmt.Println("Command not recognized")
+		fmt.Fprintln(os.Stderr, "Command not recognized")
+		usage()
 		return
 	}
 
 	cmdFunc()
 }
 
-func addCmd() {
-	if len(os.Args) < 4 {
-		exeName, err := getExeName()
-		if err != nil {
-			panic(err)
-		}
-		
-		fmt.Println("Usage:", exeName, "add <service name> <file>")
-		return
-	}
-
-	hash, err := getFileHash(os.Args[3])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Hashed file:", hash)
-
-	reqInfo := common.AddRequest{os.Args[2], hash, ""}
-	reqBytes, err := json.Marshal(reqInfo)
+func usage() {
+    exeName, err := getExeName()
 	if err != nil {
 		panic(err)
 	}
 
-	data, err := sendRequest(common.AddProtocolID, reqBytes)
-	if err != nil {
-		panic(err)
-	}
-	
-	respStr := strings.TrimSpace(string(data))
-	fmt.Println("Response:", respStr)
+	fmt.Fprintln(os.Stderr, "Usage:", exeName, "<command>")
+	fmt.Fprintln(os.Stderr, "<command> : add | get | list")
 }
-
-func getCmd() {
-	if len(os.Args) < 3 {
-		exeName, err := getExeName()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Usage:", exeName, "get <service name>")
-		return
-	}
-
-	contentHash, _, err := hashlookup.GetHash(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Response:", contentHash)
-}
-
-func listCmd() {}
 
 func getExeName() (exeName string, err error) {
 	exePath, err := os.Executable()
@@ -103,13 +64,117 @@ func getExeName() (exeName string, err error) {
 	return exeName, nil
 }
 
-func getFileHash(fileName string) (hashB58Str string, err error) {
+func addCmd() {
+    addFlags := flag.NewFlagSet("add", flag.ExitOnError)
+    fileFlag := addFlags.String("file", "", "hash file")
+	dirFlag := addFlags.String("dir", "", "hash directory")
+	stdinFlag := addFlags.Bool("stdin", false, "hash from stdin")
+	
+	addUsage := func() {
+	    exeName, err := getExeName()
+		if err != nil {
+			panic(err)
+		}
+		
+	    fmt.Fprintln(os.Stderr, "Usage:", exeName, "add [<options>] <service name>")
+	    fmt.Fprintln(os.Stderr, "<options>")
+		addFlags.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "<service name> : name of service to associate content with")
+	}
+	
+	addFlags.Usage = addUsage
+	addFlags.Parse(os.Args[2:])
+	
+	if len(addFlags.Args()) != 1 {
+		addUsage()
+		return
+	}
+	
+	var hash string = ""
+	var err error = nil
+	
+	if *fileFlag != "" {
+	    hash, err = fileHash(*fileFlag)
+	    if err != nil {
+		    panic(err)
+	    }
+	    fmt.Println("Hashed file:", hash)
+	} else if *dirFlag != "" {
+	    hash, err = directoryHash(*dirFlag)
+	    if err != nil {
+		    panic(err)
+	    }
+	    fmt.Println("Hashed directory:", hash)
+	} else if *stdinFlag {
+	    hash, err = stdinHash()
+	    if err != nil {
+		    panic(err)
+	    }
+	    fmt.Println("Hashed stdin:", hash)
+	}
+
+    fmt.Println("Adding", addFlags.Arg(0), ":", hash)
+	reqInfo := common.AddRequest{addFlags.Arg(0), hash, ""}
+	reqBytes, err := json.Marshal(reqInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := sendRequest(common.AddProtocolID, reqBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	respStr := strings.TrimSpace(string(data))
+	fmt.Println("Response:", respStr)
+}
+
+func fileHash(fileName string) (hashB58Str string, err error) {
 	fileData, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return "", err
 	}
 
 	return getHash(fileData)
+}
+
+func directoryHash(dirPathName string) (hashB58Str string, err error) {
+	allData := []byte{}
+
+	err = filepath.Walk(dirPathName,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			
+			if info.IsDir() {
+			    return nil
+			}
+			
+			fmt.Println(path)
+
+			fileData, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			allData = append(allData, fileData...)
+			return nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return getHash(allData)
+}
+
+func stdinHash() (hashB58Str string, err error) {
+	stdinData, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return "", err
+	}
+
+	return getHash(stdinData)
 }
 
 func getHash(data []byte) (hashB58Str string, err error) {
@@ -129,6 +194,26 @@ func getHash(data []byte) (hashB58Str string, err error) {
 	hashB58Str = cid.Hash().B58String()
 	return hashB58Str, nil
 }
+
+func getCmd() {
+	if len(os.Args) < 3 {
+		exeName, err := getExeName()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintln(os.Stderr, "Usage:", exeName, "get <service name>")
+		return
+	}
+
+	contentHash, _, err := hashlookup.GetHash(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response:", contentHash)
+}
+
+func listCmd() {}
 
 func sendRequest(protocolID protocol.ID, request []byte) (
 	response []byte, err error) {
