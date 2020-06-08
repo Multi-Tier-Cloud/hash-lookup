7d78a39ad21e8f3f8130cff7d11380a3257d6c88
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-06-08 00:18:10 -0400

    Add license headers

--- hl-cli/add_test.go
+++ hl-cli/add_test.go
@@ -1,8 +1,22 @@
+/* Copyright 2020 Multi-Tier-Cloud Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package main
+
 // May need to run with sudo to perform Docker tests
 // sudo env "PATH=$PATH" go test
 // In case "go" is not in sudo path
-
-package main
 
 import (
     "fmt"
